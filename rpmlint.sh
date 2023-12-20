9d63cb185bb2fb47aaf8f3f51312c67617a7217e
RJ Trujillo <eyecantcu@pm.me>
2023-12-19 20:15:34 -0700

    feat(action): Support 'checks' and 'ignore unused rpmlintrc' arguments
    Missing in 2.2.0 used previously but present in 2.4.0

--- rpmlint.sh
+++ rpmlint.sh
@@ -12,6 +12,8 @@ if [[ -n "${PRINT_CONFIG}" ]]; then ARGUMENTS+=" -p "; fi
 if [[ -n "${INSTALLED}" ]]; then ARGUMENTS+=" -i ${INSTALLED} "; fi
 if [[ -n "${TIME_REPORT}" ]]; then ARGUMENTS+=" -t "; fi
 if [[ -n "${PROFILE}" ]]; then ARGUMENTS+=" -T "; fi
+if [[ -n "${IGNORE_UNUSED_RPMLINTRC}" ]]; then ARGUMENTS+=" --ignore-unused-rpmlintrc "; fi
+if [[ -n "${CHECKS}" ]]; then ARGUMENTS+=" --checks ${CHECKS} "; fi
 if [[ -n "${STRICT}" ]]; then ARGUMENTS+=" -s "; fi
 if [[ -n "${PERMISSIVE}" ]]; then ARGUMENTS+=" -p "; fi
 
